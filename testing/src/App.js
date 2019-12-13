6e917b7ab7b20377560ddda8721081a35aa5bc96
Joey Nevarez <josephnevarez12@gmail.com>
2019-12-12 19:05:42 -0800

    refractored component architecture, should now be able to pass props

--- testing/src/App.js
+++ testing/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard';
-import Display from './components/Display';
 
 export const sum = (a, b) => a + b;
 export const subtract = (a, b) => a - b;
@@ -10,7 +9,6 @@ export const subtract = (a, b) => a - b;
 function App() {
   return (
     <div className="App">
-      <Display/>
       <Dashboard/>
     </div>
   );
--- testing/src/components/Dashboard.js
+++ testing/src/components/Dashboard.js
@@ -1,21 +1,27 @@
 import React from 'react';
+import Display from './Display';
 
 class Dashboard extends React.Component {
     constructor(){
         super()
     }
+
+
     render (){
         return (
+            <div className='appDiv'>
+                <Display/>
             <div className='buttonDiv'>
-                <div>
-                    <button>Strike</button>
-                    <button>Ball</button>
-                </div>
-                <div>
-                    <button>Foul</button>
-                    <button>Hit</button>
-                </div>
-
+            <div>
+            <button>Strike</button>
+            <button>Ball</button>
+            </div>
+            <div>
+            <button>Foul</button>
+            <button>Hit</button>
+            </div>
+            
+            </div>
             </div>
         )
     }
